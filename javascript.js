f0f1695582069efcd39ac6386d2eb0064e599a61
Isabela <isarocha0012@gmail.com>
2026-05-18 23:30:14 -0300

    Working for 1 digit number operations
    Works for onde digit operations
    Clear button still not Working

--- javascript.js
+++ javascript.js
@@ -16,7 +16,7 @@ const divide = function(num1, num2){
 
 const operate = function(num1, num2, operator){
     if (operator == '+'){
-        display.textContent = add(num1, num2);
+        return add(num1, num2);
     }
     else if (operator == '-'){
         return subtract(num1,num2);
@@ -35,35 +35,68 @@ let operator;
 let num1;
 let num2;
 
+//EVENT LISTENERS FOR THE OPERATORS
 const addBtn = document.querySelector('.addBtn');
 addBtn.addEventListener('click', () => {operator = '+'});
 
 const subBtn = document.querySelector('.subBtn');
 subBtn.addEventListener('click', () => {operator = '-'});
 
-const mulBtn = document.querySelector('.multBtn');
+const mulBtn = document.querySelector('.mulBtn');
 mulBtn.addEventListener('click', () => {operator = '*'});
 
 const divBtn = document.querySelector('.divBtn');
 divBtn.addEventListener('click', () => {operator = '/'});
 
+const eqBtn = document.querySelector('.eqBtn');
+
 let isChoosingFirst = true;
 
+//EVENT LISTENERS FOR THE NUMBERS
 const one = document.querySelector('.one');
 one.addEventListener('click', () => {chooseNumber(1)});
 
 const two = document.querySelector('.two');
 two.addEventListener('click', () => {chooseNumber(2)});
 
+const three = document.querySelector('.three');
+three.addEventListener('click', () => {chooseNumber(3)});
+
+const four = document.querySelector('.four');
+four.addEventListener('click', () => {chooseNumber(4)});
+
+const five = document.querySelector('.five');
+five.addEventListener('click', () => {chooseNumber(5)});
+
+const six = document.querySelector('.six');
+six.addEventListener('click', () => {chooseNumber(6)});
+
+const seven = document.querySelector('.seven');
+seven.addEventListener('click', () => {chooseNumber(7)});
+
+const eight = document.querySelector('.eight');
+eight.addEventListener('click', () => {chooseNumber(8)});
+
+const nine = document.querySelector('.nine');
+nine.addEventListener('click', () => {chooseNumber(9)});
+
 function chooseNumber(number){
     if (isChoosingFirst){
         num1 = number;
         isChoosingFirst = false;
-        alert(`num1: ${num1}`);
+        display.textContent = num1;
+        console.log(num1);
     }
     else{
         num2 = number;
         isChoosingFirst = true;
-        alert(`num2: ${num2}`);
-    }
-}
+        display.textContent = num2;
+        if (operator){
+            const result = operate(num1, num2, operator);
+            eqBtn.addEventListener('click', () => {
+                display.textContent = result;
+            });
+        };
+    };
+};
+
